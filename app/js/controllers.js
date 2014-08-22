62e492fe4b251a5fd7480638fe977cb84ffeb152
Franco N. Colaizzi <fnc314@gmail.com>
2014-08-22 12:52:33 -0400

    working details page with poster and full copy of results from omdbapi.com

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,10 +7,14 @@ myAppCtrl.controller('movieListCtrl', function($scope, $http) {
   $http.get('movies/movies.json').success(function(data) {
     $scope.movies = data;
   });
+
+  $scope.orderProp = "Title";
 });
 
 myAppCtrl.controller('movieDetailCtrl', function($scope, $http, $routeParams) {
-  $http.get('http://www.omdbapi.com/?i=' + $routeParams.imdbID + '&tomatoes=true&plot=full').success(function(data) {
+  $http.get('http://www.omdbapi.com/?i=' +
+  $routeParams.imdbID +
+  '&tomatoes=true&plot=full').success(function(data) {
     $scope.movie = data;
   });
 });
