7dff1d30d56510cdabf47309af602ebcdc7a0e62
Franco N. Colaizzi <fnc314@gmail.com>
2014-08-22 11:25:20 -0400

    building out secondary view of results list (what is seen when user clicks on movie title/imdb_id)

--- app/js/app.js
+++ app/js/app.js
@@ -13,9 +13,9 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/movies', {
     templateUrl: 'partials/movie-list.html',
     controller: 'movieListCtrl'});
-  $routeProvider.when('/view2', {
-    templateUrl: 'partials/partial2.html',
-    controller: 'MyCtrl2'});
+  $routeProvider.when('/movies/:Title', {
+    templateUrl: 'partials/movie-details.html',
+    controller: 'movieDetailCtrl'});
   $routeProvider.otherwise({
     redirectTo: '/movies'});
 }]);
